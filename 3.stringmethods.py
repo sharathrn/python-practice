6aa0e59f8998289a31b580c30f6d35e8bbc5dff1
Sharath Nagaraj <rnsharathcloud@gmail.com>
2026-09-24 16:38:36 +0530

    code changed_2

--- 3.stringmethods.py
+++ 3.stringmethods.py
@@ -18,3 +18,21 @@ numbers = [1, 2, 3]
 result = map(str, numbers)
 print(result)
 print(list(result))
+
+
+text = "aaaaabbbbbcccccddddddd"
+# Code to remove the Duplicates from the string
+print("".join(dict.fromkeys(text)))
+#result = ""
+#for char in text:
+#    if char not in result:
+#        result += char
+#print(result)
+
+count = {}
+
+for char in text:
+    count[char] = count.get(char, 0) + 1
+
+for char, frequency in sorted(count.items(), key=lambda x: x[1], reverse=True):
+    print(char, frequency)
--- 4.1.Collections_list.py
+++ 4.1.Collections_list.py
@@ -1,13 +1,15 @@
 models = ["gpt-4", "claude", "gemini"]
-print(models)
-print(models.append("llama"))
-print(models)
-print(models.insert(1, "mistral"))
-print(models)
-print(models[1:3])  # slice: items at index 1 and 2
+print(models) #['gpt-4', 'claude', 'gemini']
+print(models[0]) #gpt-4
+print(models[-1]) #gemini
+print(models[0:2]) #['gpt-4', 'claude']
+print(models[1:]) #['claude', 'gemini']
+print(models[:2]) #['gpt-4', 'claude']
+models.insert(1, "Mistral")
+print(models) #['gpt-4', 'Mistral', 'claude', 'gemini']
 models.remove("claude")
-print(models)
+print(models) #['gpt-4', 'Mistral', 'gemini']
 
-poped = models.pop(3)  # pop the item at index 3
-print(models)
-print(poped)
+poped = models.pop(2)  # pop the item at index 2
+print(models) #['gpt-4', 'Mistral']
+print(poped) #gemini
